615a157d44f4757b50771b75e496931f060ceaa5
gomesmarcos <gomes.marcosjf@gmail.com>
2023-04-27 19:42:49 -0300

    modulating block content

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
+        'name': 'Marcos gomeS'
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Marcos gomeS'
     })
